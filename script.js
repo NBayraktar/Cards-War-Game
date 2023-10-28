e978af0f247351d4218c393342897a5fb7f1f116
Nazim Bayraktar <nab1079@hotmail.com>
2023-10-28 18:14:26 +0100

    Add determineCardWinner function with 2 parameters c1, c2. Function supose to return text whos wins the game.

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ function drawNewCards() {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(response => response.json())
     .then(data => {
+
+      let card1 = data.cards[0].value
+      let card2 = data.cards[1].value
+
+      console.log(determineCardWinner(card1, card2))
+
       cardsContainer.children[0].innerHTML = `
         <img src="${data.cards[0].image}" class="card">
       `
@@ -23,6 +29,21 @@ function drawNewCards() {
         <img src="${data.cards[1].image}" class="card">
       `
     })
+}
+
+function determineCardWinner(c1, c2) {
+  const cardOptions = ["2", "3", "4", "5", "6", "7", "8", "9",
+    "10", "JACK", "QUEEN", "KING", "ACE"
+  ]
+  const card1ValueIndex = cardOptions.indexOf(c1)
+  const card2ValueIndex = cardOptions.indexOf(c2)
+  if (card1ValueIndex > card2ValueIndex) {
+    return `Computer Win!`
+  } else if (card1ValueIndex < card2ValueIndex) {
+    return `I Win!`
+  } else {
+    return `Game Draw - War!`
+  }
 }
 
 deckBtn.addEventListener('click', handleClick)
